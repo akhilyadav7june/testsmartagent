937b8e9ed05fed956f97547db222a6d5f44df6e3
akhilyadav7june <40736998+akhilyadav7june@users.noreply.github.com>
2018-08-15 15:20:13 +0200

    added project files

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.get("/echo", function(req, res) {
+  
+  return res.send("Test smart agent ok");
+});
+
+restService.post("/test", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.suitename
+      ? req.body.result.parameters.suitename
+      : "your test agent is started";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "testsmartsgent"
+  });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
