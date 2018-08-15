fc024046cb9a114adb5cb855e9b5d0b3fb5547ce
akhilyadav7june <40736998+akhilyadav7june@users.noreply.github.com>
2018-08-15 16:54:54 +0200

    Updated code for tfs server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const restService = express();
 
@@ -25,6 +26,34 @@ restService.post("/test", function(req, res) {
     req.body.result.parameters.suitename
       ? req.body.result.parameters.suitename
       : "your test agent is started";
+  
+  //--------------------------------------------
+  var options = {
+    hostname : 'akhileshyadav.visualstudio.com',
+    port : 443,
+    path : '/DefaultCollection/Salesforce/_apis/build/builds?api-version=2.0',
+    method : 'POST',
+    headers : {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic gpoj72wmahfytarae6vqwuztzwe5xao62e36iw2r6ujgu225b5fq'
+      },
+    body : { 'definition' : {'id' : '7'},'sourceBranch' : 'refs/heads/master' }
+};
+  
+  https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    console.log('data:', d);  
+    //process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
+  
+  //--------------------------------------------    
   return res.json({"fulfillmentMessages": [
       {
         "text": {
