620c1e61b76c993f9dd2213f36fa03b7efe289e2
akhilyadav7june <40736998+akhilyadav7june@users.noreply.github.com>
2018-08-15 23:18:25 +0200

    Updated code for build queue

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https");
+var request = require("request");
 
+var encodedPat = encodePat('gpoj72wmahfytarae6vqwuztzwe5xao62e36iw2r6ujgu225b5fq');
+var jsonbody=JSON.stringify({ definition : {id : 7},sourceBranch: "refs/heads/master" });
+
+var options = {
+   method: 'POST',
+   headers: { 'content-type': 'application/json', 'authorization': `Basic ${encodedPat}` },
+   url: 'https://akhileshyadav.visualstudio.com/DefaultCollection/Salesforce/_apis/build/builds?api-version=2.0',
+   body : jsonbody
+   
+};
+function encodePat(pat) {
+   var b = new Buffer(':' + pat);
+   var s = b.toString('base64');
+ 
+   return s;
+}
 const restService = express();
-
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -14,56 +29,53 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.get("/echo", function(req, res) {
+restService.get("/demo", function(req, res) {
   
-  return res.send("Test smart agent ok");
+  request(options, function (error, response, body) {
+	  //code=response.statusCode;
+	console.log(response.statusCode);
+  if (error) throw new Error(error);
+  
+  //console.log(body);
+  var config = JSON.parse(body);
+  console.log(config.id);
+  return res.send("Test smart agent ok"+config.id);
+});
+  
 });
 
 restService.post("/test", function(req, res) {
-  var speech =
+  var restext =
     req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.suitename
       ? req.body.result.parameters.suitename
-      : "your test agent is started";
+      : "";
   
   //--------------------------------------------
-  var options = {
-    hostname : 'akhileshyadav.visualstudio.com',
-    port : 443,
-    path : '/DefaultCollection/Salesforce/_apis/build/builds?api-version=2.0',
-    method : 'POST',
-    headers : {
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic gpoj72wmahfytarae6vqwuztzwe5xao62e36iw2r6ujgu225b5fq'
-      },
-    body : { 'definition' : {'id' : '7'},'sourceBranch' : 'refs/heads/master' }
-};
+  request(options, function (error, response, body) {
+	  //code=response.statusCode;
+	console.log(response.statusCode);
+  if (error) throw new Error(error);
   
-  https.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-
-  res.on('data', (d) => {
-    console.log('data:', d);  
-    //process.stdout.write(d);
-  });
-
-}).on('error', (e) => {
-  console.error(e);
-});
-  
-  //--------------------------------------------    
+  //console.log(body);
+  var config = JSON.parse(body);
+  console.log(config.id);
+  restext="Congrats, Build "+restext+" is started."
   return res.json({"fulfillmentMessages": [
       {
         "text": {
           "text": [
-            speech
+            restext
           ]
         }
       }
     ]
   });
+});
+  
+  //--------------------------------------------    
+  
 });
 
 restService.listen(process.env.PORT || 8000, function() {
